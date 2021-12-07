7197d249bd34b732e8f0ad6cb928b045b0933124
Coyote-31 <coyotepouil@gmail.com>
2021-12-07 12:10:04 +0100

    first commit

---
+++ src/test/java/com/coyote/big_city_library/rest_server/RestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coyote.big_city_library.rest_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
