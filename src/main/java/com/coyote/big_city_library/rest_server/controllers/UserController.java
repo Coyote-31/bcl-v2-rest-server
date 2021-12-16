5b5f4a3152c463cc4dcc8cf661358250fbecd044
Coyote-31 <coyotepouil@gmail.com>
2021-12-16 15:03:06 +0100

    🕹 Update UserRepository to use UserService

--- src/main/java/com/coyote/big_city_library/rest_server/controllers/UserController.java
+++ src/main/java/com/coyote/big_city_library/rest_server/controllers/UserController.java
@@ -1,24 +1,30 @@
 package com.coyote.big_city_library.rest_server.controllers;
 
+import java.util.List;
+
 import com.coyote.big_city_library.rest_server.dao.entities.User;
-import com.coyote.big_city_library.rest_server.dao.repositories.UserRepository;
+import com.coyote.big_city_library.rest_server.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @GetMapping("/all")
-    public Iterable<User> getAllUser() {
-        
-        return userRepository.findAll();
+    public List<User> findAllUsers() {
+        List<User> users = userService.findAllUsers();
+        log.debug("findAllUsers : {} user(s) found.", users.size());
+        return users;
     }
     
 }
