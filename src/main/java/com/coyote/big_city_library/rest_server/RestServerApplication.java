56925714ded8637861f8f65483af4a88b03acc19
Coyote-31 <coyotepouil@gmail.com>
2022-01-13 13:51:59 +0100

    🔥 Remove Rest tag

--- src/main/java/com/coyote/big_city_library/rest_server/RestServerApplication.java
+++ src/main/java/com/coyote/big_city_library/rest_server/RestServerApplication.java
@@ -2,10 +2,8 @@ package com.coyote.big_city_library.rest_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class RestServerApplication {
 
 	public static void main(String[] args) {
