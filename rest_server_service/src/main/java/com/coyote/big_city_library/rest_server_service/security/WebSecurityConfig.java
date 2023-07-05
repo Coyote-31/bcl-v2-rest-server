188d37073e7f52dd7b1bd1126776d2fe3d84bf4f
Coyote-31 <coyotepouil@gmail.com>
2023-07-05 20:13:56 +0200

    security: Update WebSecurityConfig
    - To match Spring boot v2.7.13

--- rest_server_service/src/main/java/com/coyote/big_city_library/rest_server_service/security/WebSecurityConfig.java
+++ rest_server_service/src/main/java/com/coyote/big_city_library/rest_server_service/security/WebSecurityConfig.java
@@ -1,34 +1,30 @@
 package com.coyote.big_city_library.rest_server_service.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebMvc
+public class WebSecurityConfig {
 
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private JwtFilter jwtFilter;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder,
+            CustomUserDetailsService userDetailsService) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                   .userDetailsService(userDetailsService)
+                   .passwordEncoder(passwordEncoder)
+                   .and()
+                   .build();
     }
 
     // Roles declaration
@@ -37,27 +33,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String ROLE_EMPLOYEE = "EMPLOYEE";
     private static final String ROLE_USER = "USER";
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                // .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests()
-                .antMatchers("/auth/login", "/books/search").permitAll()
-                .antMatchers("/libraries", "/loans/user/**", "/loans/extend/**")
-                .hasAnyAuthority(ROLE_USER, ROLE_ADMIN)
-                .antMatchers("/loans/add", "/loans/add/partial", "/loans/update")
-                .hasAnyAuthority(ROLE_EMPLOYEE, ROLE_ADMIN)
-                .antMatchers("/loans/batch/**").hasAnyAuthority(ROLE_BATCH, ROLE_ADMIN)
-                .anyRequest().hasAuthority(ROLE_ADMIN)
-                .and()
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-    }
-
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    protected SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
+        http.cors(cors -> cors.disable())
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(
+                    sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(
+                    ahr -> ahr.mvcMatchers("/auth/login", "/books/search")
+                              .permitAll()
+                              .mvcMatchers("/libraries", "/loans/user/**", "/loans/extend/**")
+                              .hasAnyAuthority(ROLE_USER, ROLE_ADMIN)
+                              .mvcMatchers("/loans/add", "/loans/add/partial", "/loans/update")
+                              .hasAnyAuthority(ROLE_EMPLOYEE, ROLE_ADMIN)
+                              .mvcMatchers("/loans/batch/**")
+                              .hasAnyAuthority(ROLE_BATCH, ROLE_ADMIN)
+                              .anyRequest()
+                              .hasAuthority(ROLE_ADMIN))
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
 }
